33e12176ee5fa801a3bbb4a78512d78875a0e2cd
Sameer Rahmani <lxsameer@gnu.org>
2024-09-30 11:05:46 +0100

    Fix the incomplete condition on mk-derivation

--- pkgs/stdenv/generic/make-derivation.nix
+++ pkgs/stdenv/generic/make-derivation.nix
@@ -188,8 +188,8 @@ in rec {
            "__darwinAllowLocalNetworking"
            "__impureHostDeps" "__propagatedImpureHostDeps"
            "sandboxProfile" "propagatedSandboxProfile"])
-        // (lib.optionalAttrs (!(attrs ? name) && attrs ? pname && attrs ? version)) {
-          name = builtins.trace ">>> "${attrs.pname ? "0"}-${attrs.version ? "1"} "${attrs.pname}-${attrs.version}";
+        // (lib.optionalAttrs (!(attrs ? name) && attrs ? pname && (attrs ? version && attrs.version != null)) {
+          name = builtins.trace ">>> ${attrs.pname ? "0"}" "${attrs.pname}-${attrs.version}";
         } // (lib.optionalAttrs (stdenv.hostPlatform != stdenv.buildPlatform && !dontAddHostSuffix && (attrs ? name || (attrs ? pname && attrs ? version)))) {
           # Fixed-output derivations like source tarballs shouldn't get a host
           # suffix. But we have some weird ones with run-time deps that are
